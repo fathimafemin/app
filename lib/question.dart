03f655180a93b8f9774e32ad647bc1a41b7175bd
ahladhh <ahladhthomas@gmail.com>
2024-02-28 17:11:35 +0530

    update

--- lib/question.dart
+++ lib/question.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:quiz/quest.dart';
 
 class Quistion extends StatefulWidget {
   const Quistion({Key? key}) : super(key: key);
@@ -8,11 +9,85 @@ class Quistion extends StatefulWidget {
 }
 
 class _QuistionState extends State<Quistion> {
+  List quest = [
+    Quiz(quis: 'College is for study', ans: true),
+    Quiz(quis: 'Cat is a domestic animal', ans: true),
+    Quiz(quis: 'Sky color is blue', ans: true),
+    Quiz(quis: 'Book is made by paper', ans: true),
+    Quiz(quis: 'cat have 5 legs', ans: false),
+    Quiz(quis: 'Banana is yellow in color', ans: true),
+    Quiz(quis: 'Banana is a fruit', ans: true),
+    Quiz(quis: 'Grapes is a vegetable', ans: false),
+    Quiz(quis: 'Mango is a food', ans: true),
+    Quiz(quis: 'mango is a fruit', ans: true),
+  ];
+  int index = 0;
+  String result =' ';
+  void next() {
+    if (index < quest.length) {
+      setState(() {
+        index++;
+      });
+    }
+  }
+  void correct(bool answer){
+      print(answer);
+      if(quest[index].ans==answer){
+        result = 'Answer correct';
+      }
+      else{
+       result = 'wrong answer';
+      }
+
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Colors.black,
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Center(
+            child: Text(quest[index].quis,
+                style: TextStyle(
+                  color: Colors.cyan.withOpacity(0.8),
+                  height: 12,
+                  fontSize: 34,
+                )),
+          ),
+          Container(
+            color: Colors.black,
+            width: 150,
+            child: TextButton(
+                onPressed: () {
+                  correct(true);
+                  next();
+                },
+                child: Text(
+                  'YES',
+                  style: TextStyle(color: Colors.white),
+                )),
+          ),
+          SizedBox(
+            height: 30,
+          ),
+          Container(
+            color: Colors.black,
+            width: 150,
+            child: TextButton(
+                onPressed: () {
+                  correct(false);
+                  next();
+                },
+                child: Text(
+                  'NO',
+                  style: TextStyle(color: Colors.white),
+                )),
+          ),
+          Text(result,style: TextStyle(color: Colors.indigo),)
+        ],
+      ),
     );
   }
 }
-
